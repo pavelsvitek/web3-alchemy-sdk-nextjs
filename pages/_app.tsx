7c6c475db337229970e98642ad8068474b3207f3
pavel-lens <988706+pavel-lens@users.noreply.github.com>
2024-05-02 21:53:41 +0200

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,20 +10,18 @@ import {
   mainnet,
   optimism,
   polygon,
-  sepolia,
 } from 'wagmi/chains';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
 const config = getDefaultConfig({
   appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  projectId: 'web3-project-assignment',
   chains: [
     mainnet,
     polygon,
     optimism,
     arbitrum,
     base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
   ssr: true,
 });
