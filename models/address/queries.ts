d5194e3d7de11b1028a4a222d6dd95117aa4c8d5
pavel-lens <988706+pavel-lens@users.noreply.github.com>
2024-05-05 21:30:06 +0200

    fix return type useAddressERC20Tokens()

--- models/address/queries.ts
+++ models/address/queries.ts
@@ -2,7 +2,10 @@ import { UseQueryOptions, UseQueryResult, useQuery } from '@tanstack/react-query
 import { fetchAddressERC20Tokens } from './loaders';
 import { TokenBalanceViewModel } from '../../src/services/alchemy.service';
 
-export function useAddressERC20Tokens(address: string, options: Partial<UseQueryOptions> = {}) {
+export function useAddressERC20Tokens(
+  address: string,
+  options: Partial<UseQueryOptions<Awaited<ReturnType<typeof fetchAddressERC20Tokens>>>> = {}
+) {
   return useQuery({
     ...options,
     queryFn: () => fetchAddressERC20Tokens(address),
