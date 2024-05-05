294620d99802a9574a76c0a1505be0cc590ca762
pavel-lens <988706+pavel-lens@users.noreply.github.com>
2024-05-05 20:37:59 +0200

    implement alchemyService.getERC721TokenListViewModel() which fetches token metadata and calculates balance using token decimals

--- pages/api/address/tokens.ts
+++ pages/api/address/tokens.ts
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   const alchemyService = new AlchemyService();
-  const tokens = await alchemyService.getERC20TokenList(session.address);
+  const erc20Tokens = await alchemyService.getERC721TokenListViewModel(session.address);
 
-  return res.json({ address: session.address, tokens });
+  return res.json({ address: session.address, erc20Tokens });
 }
--- src/services/alchemy.service.ts
+++ src/services/alchemy.service.ts
@@ -1,6 +1,12 @@
-import { Alchemy, TokenBalancesResponseErc20 } from 'alchemy-sdk';
+import { Alchemy, TokenBalancesResponseErc20, TokenMetadataResponse } from 'alchemy-sdk';
 
 export class AlchemyServiceError extends Error {}
+
+export type TokenBalanceViewModel = {
+  name: string;
+  symbol: string;
+  balance: number;
+};
 
 export class AlchemyService {
   private apiKey: string;
@@ -23,6 +29,48 @@ export class AlchemyService {
 
       return erc20TokenList;
     } catch (err) {
+      throw new AlchemyServiceError(`Error fetching ERC20 token list for address ${address}: ${err}`);
+    }
+  }
+
+  async getERC20TokenMetadata(contractAddress: string): Promise<TokenMetadataResponse> {
+    try {
+      const metadata = await this.alchemyClient.core.getTokenMetadata(contractAddress);
+
+      return metadata;
+    } catch (err) {
+      throw new AlchemyServiceError(`Error fetching token metadata for contract address ${contractAddress}: ${err}`);
+    }
+  }
+
+  async getERC721TokenListViewModel(address: string): Promise<TokenBalanceViewModel[]> {
+    try {
+      const tokenList = await this.getERC20TokenList(address);
+      const nonZeroBalances = tokenList.tokenBalances.filter((token) => token.tokenBalance !== '0');
+
+      const tokenBalances: TokenBalanceViewModel[] = [];
+      for (const token of nonZeroBalances) {
+        const metadata = await this.getERC20TokenMetadata(token.contractAddress);
+        if (!metadata.decimals || !metadata.symbol) {
+          console.info(`Token metadata for contract "${token.contractAddress}": ${JSON.stringify(metadata)}`);
+          console.error(`Token metadata for contract address ${token.contractAddress} is missing decimals or symbol`);
+          continue;
+        }
+
+        const balance = Number(token.tokenBalance) / Math.pow(10, metadata.decimals);
+
+        console.log(`${metadata.name}: ${balance.toFixed(2)} ${metadata.symbol}`);
+
+        tokenBalances.push({
+          name: metadata.name ?? metadata.symbol,
+          symbol: metadata.symbol,
+          balance,
+        });
+      }
+
+      return tokenBalances;
+    } catch (err) {
+      console.error(`Error fetching ERC20 token list for address ${address}: ${err}`);
       throw new AlchemyServiceError(`Error fetching ERC20 token list for address ${address}: ${err}`);
     }
   }
