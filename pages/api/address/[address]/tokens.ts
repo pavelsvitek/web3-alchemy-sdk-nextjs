550200ee0d5d3a931854553d363b7f3cbdc444c2
pavel-lens <988706+pavel-lens@users.noreply.github.com>
2024-05-05 21:51:17 +0200

    verify EVM address format in API

--- pages/api/address/[address]/tokens.ts
+++ pages/api/address/[address]/tokens.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getServerSession } from 'next-auth';
 import { AlchemyService } from '../../../../src/services/alchemy.service';
+import { utils } from 'ethers';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
@@ -8,6 +8,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   const address = req.query.address as string;
+
+  if (utils.isAddress(address) === false) {
+    return res.status(400).json({ message: 'Invalid EVM address format' });
+  }
 
   const alchemyService = new AlchemyService();
   const erc20Tokens = await alchemyService.getERC721TokenListViewModel(address);
